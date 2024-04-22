1f3870f2b89bec5220644372d34bac479e6b63cb
Bruno <bganontha@gmail.com>
2024-04-22 16:59:59 +0100

    Refactor theme and add new colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff15c5ff);
+  static const Color backgroundColor = Color(0xffe9e7e7);
+  static const Color secondaryColor = Color(0xffffffff);
+  static const Color textColor = Color(0x00000000);
+  static const Color primaryTextColor = Color(0xffffffff);
+  static const Color secondaryTextColor = Color(0xff15c5ff);
+  static const Color iconColor1 = Color(0xff15c5ff);
+  static const Color iconColor2 = Color(0xffdbd9d9);
+}
