4615402db9ee2100ef14703031f3a9196374242b
Bruno <bganontha@gmail.com>
2024-04-24 11:14:24 +0100

    Refactor IconWithName widget and update PopularProperty layout

--- lib/widgets/icon_with_name.dart
+++ lib/widgets/icon_with_name.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
 class IconWithName extends StatelessWidget {
-  final Icon icon;
+  final IconData icon;
   final int number;
   final String name;
-  const IconWithName(
-      {super.key,
-      required this.number,
-      required this.name,
-      required this.icon});
+  final Color? color;
+  const IconWithName({
+    super.key,
+    required this.number,
+    required this.name,
+    required this.icon,
+    this.color = Colors.black45,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        icon,
+        Icon(
+          icon,
+          color: color,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [Text('$number'), Text(name)],
--- lib/widgets/popular_property.dart
+++ lib/widgets/popular_property.dart
@@ -1,3 +1,5 @@
+import 'package:belle_house_mobile_app/utils/colors.dart';
+import 'package:belle_house_mobile_app/widgets/big_text.dart';
 import 'package:belle_house_mobile_app/widgets/icon_with_name.dart';
 import 'package:flutter/material.dart';
 
@@ -23,63 +25,94 @@ class PopularProperty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 270,
       width: MediaQuery.of(context).size.width * 0.7,
-      child: Column(
-        children: [
-          //image with like (heart) button in a stack widget
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: image,
-              ),
-              Positioned(
-                  right: 8,
-                  top: 8,
-                  child: IconButton(
-                    icon: Icon(Icons.favorite_border),
-                    onPressed: () {},
-                  ))
-            ],
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10),
           ),
+          color: AppColors.secondaryColor,
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.secondaryColor.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            //image with like (heart) button in a stack widget
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  child: image,
+                ),
+                Positioned(
+                    left: 2,
+                    top: 2,
+                    child: IconButton(
+                      icon: Icon(Icons.favorite_border),
+                      onPressed: () {},
+                    ))
+              ],
+            ),
 
-          //property details
-          Column(
-            children: [
-              //price and type
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text(type), Text('$price')],
-              ),
-              const SizedBox(height: 6),
-              //area and city
-              Row(
-                children: [
-                  const Icon(
-                    Icons.location_on,
-                  ),
-                  Text(city),
-                  const Text('-'),
-                  Text(area)
-                ],
-              ),
-              //bed and bedrooms
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconWithName(
-                    number: 2,
-                    name: 'Chambres',
-                    icon: Icon(Icons.bed),
-                  ),
-                  IconWithName(
-                      number: 03, name: 'Toilettes', icon: Icon(Icons.shower))
-                ],
-              )
-            ],
-          )
-        ],
+            //property details
+            Column(
+              children: [
+                //price and type
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    BigText(
+                      text: '$type',
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                    BigText(
+                      text: '$price FCFA',
+                      color: AppColors.iconColor1,
+                      fontSize: 16,
+                    )
+                  ],
+                ),
+                const SizedBox(height: 6),
+                //area and city
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.location_on,
+                      color: AppColors.iconColor1,
+                    ),
+                    Text(city),
+                    const Text('-'),
+                    Text(area)
+                  ],
+                ),
+                //bed and bedrooms
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconWithName(
+                      number: 2,
+                      name: 'Chambres',
+                      icon: Icons.bed,
+                    ),
+                    IconWithName(
+                        number: 03, name: 'Toilettes', icon: Icons.shower)
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
