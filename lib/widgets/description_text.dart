b846e225e12f47d9ff568f028c482c80932e501e
Bruno <bganontha@gmail.com>
2024-05-01 15:14:52 +0100

    Refactor layout and update DescriptionText widget in description_text.dart

--- lib/widgets/description_text.dart
+++ lib/widgets/description_text.dart
@@ -1,3 +1,4 @@
+import 'package:belle_house_mobile_app/utils/colors.dart';
 import 'package:belle_house_mobile_app/utils/dimensions.dart';
 import 'package:belle_house_mobile_app/widgets/small_text.dart';
 import 'package:flutter/material.dart';
@@ -43,11 +44,19 @@ class _DescriptionTextState extends State<DescriptionText> {
                         : firstHalfText + secondHalfText),
                 InkWell(
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Text(
-                        hiddenText ? '...more' : ' less',
-                        style: TextStyle(color: Colors.blue),
+                        hiddenText ? '...voir plus' : ' voir moins',
+                        style: const TextStyle(
+                            color: AppColors.primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        hiddenText
+                            ? Icons.arrow_drop_down
+                            : Icons.arrow_drop_up,
+                        color: AppColors.primaryColor,
                       ),
                     ],
                   ),
