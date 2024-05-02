543ac1fbc1946bd8008af95691488dcc7d37b259
Bruno <bganontha@gmail.com>
2024-05-02 21:19:55 +0100

    load dependencies inside inti method

--- lib/helpers/dependencies.dart
+++ lib/helpers/dependencies.dart
@@ -1,1 +1,13 @@
-Future<void> init() async {}
+import 'package:belle_house_mobile_app/controllers/popular_products_controller.dart';
+import 'package:belle_house_mobile_app/data/api/api_client.dart';
+import 'package:belle_house_mobile_app/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+
+Future<void> init() async {
+  //load apiclient
+  Get.lazyPut(() => ApiClient(appBaseUrl: 'appBaseUrl'));
+  //load repo
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+  //controller
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
+}
